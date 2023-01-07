c51cc8766461c8e7f372dd85f7263ddc96300a36
antshar <54121465+antshar@users.noreply.github.com>
2023-01-07 22:49:02 +0200

    Updated to use Page Objects

--- spec/loginSpec.ts
+++ spec/loginSpec.ts
@@ -1,7 +1,7 @@
-import 'jasmine';
+import 'jasmine-reporters';
 
 // importing custom class for testing
-import { Test } from '../loginClass';
+import { LoginPage, MainPage } from '../pageClass';
 
 //to set jasmine default timeout
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 20 * 1000;
@@ -12,25 +12,29 @@ describe("Authorize, add, remove user test scenario", function () {
 	describe("Selenium test case for login page", function () {
 		it("takes username and password for logging in", async function () {
 
-			expect(await Test.login()).toBe('success');
+			const loginPage = new LoginPage();
+
+			expect(await loginPage.login()).toBe('success');
 
 		});
 	});
 
 	// user instance with name and password
-	const user = new Test("Alice", "Qwerty123_");
+	const mainPage = new MainPage();
+	const username = "Alice";
+	const password = "Qwerty123_";
 
 	describe("Create user scenario", function () {
 		it("Presses add button, fills in the data and tries to add the person", async function () {
 
-			expect(await user.addPerson()).toBe('success');
+			expect(await mainPage.addPerson(username, password)).toBe('success');
 		});
 	});
 
 	describe("Remove user scenario", function () {
 		it("Presses add button, fills in the data and tries to add the person", async function () {
 
-			expect(await user.removePerson()).toBe('success');
+			expect(await mainPage.removePerson(username)).toBe('success');
 		});
 	});
 
